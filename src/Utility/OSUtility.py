0d91f0d796c8d38c81582db77bf2ca56cc284d02
Abhinav Misra <am13@iitbbs.ac.in>
2023-10-07 13:25:05 +0530

    refactor and simplify OSUtility

--- src/Utility/OSUtility.py
+++ src/Utility/OSUtility.py
@@ -1,38 +1,19 @@
 import platform
 
-
 class OSUtility:
     def __init__(self):
         self.currentOS = platform.system()
         self.architecture = platform.architecture()[0]
         self.machineType = platform.machine().lower()
-        self.osSuffix = None
+        self.osSuffix, self.osFriendlyShortName = self.getCurrentOSInfo()
 
-
-    def getCurrentOSConfigKey(self):
-        if self.currentOS == "Linux":
-            if "aarch64" in self.machineType or "arm" in self.machineType:
-                self.osSuffix = 'linux-arm64'
-            elif "64" in self.architecture:
-                self.osSuffix = 'linux64'
-        elif self.currentOS == "Darwin":
-            if "arm" in self.machineType:
-                self.osSuffix = 'mac-arm64'
-            elif "64" in self.architecture:
-                self.osSuffix = 'mac-x64'
-        elif self.currentOS == "Windows":
-            if "64" in self.architecture:
-                self.osSuffix = 'win64'
-            elif "32" in self.architecture:
-                self.osSuffix = 'win32'
-        return self.osSuffix
-
-
-    def getCurrentOS(self):
-        if self.currentOS == "Darwin":
-            self.osSuffix = 'mac'
-        elif self.currentOS == "Linux":
-            self.osSuffix = 'linux'
-        elif self.currentOS == "Windows":
-            self.osSuffix = 'win'
-        return self.osSuffix
+    def getCurrentOSInfo(self):
+        os_info = {
+            ("Linux", "64bit"): ("linux64", "Linux"),
+            ("Linux", "arm64"): ("linux-arm64", "Linux"),
+            ("Darwin", "64bit"): ("mac-x64", "macOS"),
+            ("Darwin", "arm64"): ("mac-arm64", "macOS"),
+            ("Windows", "64bit"): ("win64", "Windows"),
+            ("Windows", "32bit"): ("win32", "Windows")
+        }
+        return os_info.get((self.currentOS, self.architecture), (None, None))
