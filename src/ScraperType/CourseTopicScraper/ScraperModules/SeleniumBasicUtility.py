6868fdb85a6ad440dff411a174254ef028c90ff9
Anilabha Datta <anilabhadatta@gmail.com>
2025-05-02 23:02:46 +0530

    cloudlab fix

--- src/ScraperType/CourseTopicScraper/ScraperModules/SeleniumBasicUtility.py
+++ src/ScraperType/CourseTopicScraper/ScraperModules/SeleniumBasicUtility.py
@@ -63,7 +63,7 @@ class SeleniumBasicUtility:
                             EC.visibility_of_element_located((By.XPATH, generalPageSelector)))
                 except Exception as e:
                     WebDriverWait(self.browser, self.timeout + 5).until(
-                        EC.visibility_of_element_located((By.CSS_SELECTOR, cloudLabPageSelector)))
+                        EC.visibility_of_element_located((By.XPATH, cloudLabPageSelector)))
             except Exception as e:
                 lineNumber = e.__traceback__.tb_lineno
                 self.logger.error(f"SeleniumBasicUtility:waitWebdriverToLoadTopicPage: {lineNumber}: {e}")
