046072d3b418c847728908bc6f8a7d978ef98996
Anilabha Datta <anilabhadatta@gmail.com>
2026-06-06 23:15:04 +0530

    Update EducativeScraper.py

--- EducativeScraper.py
+++ EducativeScraper.py
@@ -104,7 +104,7 @@ class EducativeScraper:
 
 
 if __name__ == '__main__':
-    version = "v4.0.32 Dev Branch"
+    version = "v4.0.33 Dev Branch"
     helpDescription = f"""
                         Educative Scraper ({version}), developed by Anilabha Datta
                         Project Link: https://github.com/anilabhadatta/educative.io_scraper/
