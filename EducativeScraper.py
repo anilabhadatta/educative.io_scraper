32734d770fb606c42ef553f04bcb3157a4517fc7
Anilabha Datta <anilabhadatta@gmail.com>
2026-06-08 03:09:46 +0530

    feat: implement initial scraper infrastructure and add exploratory testing scripts

--- EducativeScraper.py
+++ EducativeScraper.py
@@ -104,7 +104,7 @@ class EducativeScraper:
 
 
 if __name__ == '__main__':
-    version = "v4.0.34 Dev Branch"
+    version = "v4.0.36 Dev Branch"
     helpDescription = f"""
                         Educative Scraper ({version}), developed by Anilabha Datta
                         Project Link: https://github.com/anilabhadatta/educative.io_scraper/
