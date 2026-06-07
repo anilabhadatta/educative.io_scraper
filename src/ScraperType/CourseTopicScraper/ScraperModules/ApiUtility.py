0c2aef31430872cc32c51460a3fff22ec8b91e52
Anilabha Datta <anilabhadatta@gmail.com>
2026-06-08 04:33:39 +0530

    feat: add PublicContentScraper to support scraping of Educative blog, newsletter, and answer pages

--- src/ScraperType/CourseTopicScraper/ScraperModules/ApiUtility.py
+++ src/ScraperType/CourseTopicScraper/ScraperModules/ApiUtility.py
@@ -84,6 +84,7 @@ class ApiUtility:
                 fetch("{url}", {{
                     method: 'GET',
                     mode: 'cors',
+                    cache: 'no-store',
                     headers: {{
                         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7'
                     }}
