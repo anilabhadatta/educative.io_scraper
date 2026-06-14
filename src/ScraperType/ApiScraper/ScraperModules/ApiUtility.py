61185555517cc3e37c84aa9603260c9bca406565
Anilabha Datta <anilabhadatta@gmail.com>
2026-06-14 17:47:15 +0530

    IF NO PAGE SKIP, ADDED SPECIAL TOPIC SKIP DETECTION
    # Conflicts:
    #	README.md

--- src/ScraperType/ApiScraper/ScraperModules/ApiUtility.py
+++ src/ScraperType/ApiScraper/ScraperModules/ApiUtility.py
@@ -188,7 +188,7 @@ class ApiUtility:
             self.logger.info("Getting Course url")
             try:
                 self.browser.get(textFileUrl)
-                self.osUtils.sleep(3)
+                self.osUtils.sleep(10)
             except:
                 self.logger.info("Page Loading Issue, pressing ESC to stop page load")
                 self.browser.execute_script("window.stop();")
